edb3b0189865c79d564195de1edd243df7684cc1
gleonio <gleonio@gmail.com>
2025-03-08 12:11:05 -0800

    installed winston and created a request logger and error logger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mainRouter = require("./routes/index");
 const { errors } = require("celebrate");
 const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express(); // Initialize the Express application
 const { PORT = 3001 } = process.env; // Set the port for the server
@@ -17,7 +18,9 @@ mongoose
 
 app.use(express.json()); // Middleware to parse JSON bodies. place before calling routes
 app.use(cors());
+app.use(requestLogger); // enable the request logger before all route handlers
 app.use("/", mainRouter); // Use the main router for all routes
+app.use(errorLogger); // enalbe the error logger after the route handlers and before the error handlers
 app.use(errors()); // Celebrate error handler
 app.use(errorHandler); // Centralized error handler
 
