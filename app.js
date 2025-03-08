f0e000a1d4f298008824a6e29c210849b6b409bf
gleonio <gleonio@gmail.com>
2025-03-07 17:54:24 -0800

    created validation functions for inbound server data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
+const { errors } = require("celebrate");
 const errorHandler = require("./middlewares/error-handler");
 
 const app = express(); // Initialize the Express application
@@ -17,7 +18,8 @@ mongoose
 app.use(express.json()); // Middleware to parse JSON bodies. place before calling routes
 app.use(cors());
 app.use("/", mainRouter); // Use the main router for all routes
-app.use(errorHandler);
+app.use(errors()); // Celebrate error handler
+app.use(errorHandler); // Centralized error handler
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
